46f597ea9a69aef27c5f13777a4bf359b8319670
Keith <kqbui@csu.fullerton.edu>
2025-04-27 03:21:30 -0700

    changed ContentView to LoginView, implemented first screen of our mockup

---
+++ ktrecipeapp/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ktrecipeapp
+//
+//  Created by csuftitan on 4/27/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
